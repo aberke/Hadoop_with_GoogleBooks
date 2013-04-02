8c51dcaab889828df6ea65a603c0a4a5e5ad44c1
Matt Patenaude <matt@mattpatenaude.com>
2013-04-01 21:46:47 -0400

    Replaced spaces with tabs

--- question1b.py
+++ question1b.py
@@ -6,19 +6,19 @@ match_year = 1987
 # input: Google-books bigram data
 def max_bigram_year(argv):
 	for line in sys.stdin:
-        try:
-    		[bigram, year, count, page_count, book_count] = line.split('\t')
-        except ValueError:
-            print "Error with -- %s" % line
-            raise
-        
+		try:
+			[bigram, year, count, page_count, book_count] = line.split('\t')
+		except ValueError:
+			print "Error with -- %s" % line
+			raise
+		
 		if year == str(match_year):
 			print "LongValueMax:" + bigram + "\t" + count
 
 
 def main(argv): 
-    max_bigram_year(argv) #change on what we're using it for I suppose until we break this into multiple files
+	max_bigram_year(argv) #change on what we're using it for I suppose until we break this into multiple files
 
 
 if __name__ == "__main__": 
-    main(sys.argv) 
+	main(sys.argv) 
