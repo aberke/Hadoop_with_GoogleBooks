43e22d47a69a1a11f06154649d5bf5858ff52182
Matt Patenaude <matt@mattpatenaude.com>
2013-04-02 21:51:15 -0400

    Cleaned up queries for question 3

--- question3.sql
+++ question3.sql
@@ -1,19 +1,3 @@
--- ********************** For each new job: ***********************************************
--- **************************************************************************************** 
--- start a job flow with this command.
-elastic-mapreduce --create --alive --hive-interactive --hive-versions 0.7
---This will give you a job flow ID. You can track the status of your job low with the list parameter.
-elastic-mapreduce --list <job-flow-id>
---In a few minutes this will start and enter the waiting state, at which point we can SSH to the master node.
-elastic-mapreduce --ssh <job-flow-id>
---Now that we have Hive installed on our cluster we can log into its shell by typing 'hive'.
-$ hive
---There are two settings that we need to set in order to efficiently process the data from Amazon S3. Type these two commands into the Hive shell.
-hive> set hive.base.inputformat=org.apache.hadoop.hive.ql.io.HiveInputFormat;
-hive> set mapred.min.split.size=134217728;
--- ****************************************************************************************
-
-
 -- ################## problem 3: #####################################################################################################
 --  Suppose we're interesting in finding trigrams which tend to appear many times on the same page or many times in the same book.
 --	a) What is the trigram that appears at least 10 times with the lowest ratio of page_count to total_count?
@@ -44,17 +28,22 @@ CREATE TABLE question3_firstpass (
 INSERT OVERWRITE TABLE question3_firstpass
 SELECT
  gram,
- SUM(page_count),
- SUM(book_count),
- SUM(total_count)
+ SUM(pages),
+ SUM(books),
+ SUM(occurrences)
 FROM
- english_trigrams
+ trigrams
+GROUP BY
+ gram
 DISTRIBUTE BY
  gram;
--- GROUP BY -- don't need, right??
---  gram;
 
+CREATE TABLE question3_pageratio (
+    gram string,
+    ratio double
+);
 
+INSERT OVERWRITE TABLE question3_pageratio
 SELECT
  gram,
  page_count / total_count as ratio
@@ -69,6 +58,13 @@ LIMIT
 -- ********************** 3.a ###############################
 -- //////////////////////////////////////////////////////////
 -- ********************** 3.b ###############################
+
+CREATE TABLE question3_bookratio (
+    gram string,
+    ratio double
+);
+
+INSERT OVERWRITE TABLE question3_bookratio
 SELECT
  gram,
  book_count / total_count as ratio
@@ -84,3 +80,4 @@ SORT BY
  ratio ASC
 LIMIT
  5;
+ 
